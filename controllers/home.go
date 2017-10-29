216c4e816887aa58a61964b27d44b97df9ff3666
root <root@xiehui.(none)>
2017-10-29 23:10:49 +0800

    Delete sub directory's .gitignore

---
+++ controllers/home.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+    "log"
+    "github.com/adairxie/blog/models"
+    "github.com/astaxie/beego"
+)
+
+type HomeController struct {
+    beego.Controller
+}
+
+func (this *HomeController) Get() {
+    this.TplName = "index.html"
+
+    /*article := &models.Article{
+        Title:"Man must explore, and this is exploration at its greatest",
+        SubTitle:"Problems look mighty small from 150 miles up",
+        Created: time.Now(),
+    }
+
+    err := models.AddArticle(article)
+    if err != nil {
+        log.Fatalf("Add article failed: %s", err)
+    }*/
+
+    articles, err := models.GetAllArticles()
+    if err != nil {
+        log.Fatalf("Get all articles err: %s", err)
+    }
+    
+    this.Data["Articles"] = articles
+}
